3df9cfb65a124b493d40947d662ff1a348b2ebb1
fedoseevatatyana <74595766+fedoseevatatyana@users.noreply.github.com>
2020-11-26 22:37:47 +0300

    Add files via upload

---
+++ Main.java
@@ -0,0 +1,7 @@
+public class Main {
+    public static void main(String[] args) {
+        int ticket_price = 10051;
+        int miles = ticket_price/20;
+        System.out.println(miles);
+    }
+}
